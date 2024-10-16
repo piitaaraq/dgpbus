626f7829d171868f292100eece2815b01f43d7ff
Piitaaraq <peter.lovstrom@gmail.com>
2024-10-16 18:18:04 +0200

    Prepared for deployment

--- dgp_bus/dgp_bus_project/wsgi.py
+++ dgp_bus/dgp_bus_project/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dgp_bus.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dgp_bus_project.settings')
 
 application = get_wsgi_application()
